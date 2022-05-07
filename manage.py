057f6057d841c26240d58f45e98b2ac1e0cd6253
charles okunzo <okunzopeter01@gmail.com>
2022-05-08 00:57:18 +0300

    feature:install migrate

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from app.models import User
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app('development')
@@ -11,6 +12,9 @@ manager.add_command('server', Server)
 @manager.shell
 def make_shell_context():
   return dict(app = app, db = db, User = User)
+
+migrate = Migrate(app)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
