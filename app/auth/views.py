8ba5aa2b021a996bb6f80034c599b4a77f8ed232
charles okunzo <okunzopeter01@gmail.com>
2022-05-08 18:45:32 +0300

    feature:add user registration route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,25 @@
-from flask import render_template
+from crypt import methods
+from turtle import title
+from flask import render_template, redirect, url_for
+from .forms import RegistrationForm
+from ..models import User
+from .. import db
 from . import auth
 
 
 @auth.route('/login')
 def login():
   return render_template('auth/login.html')
+
+
+
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username = form.username.data, email = form.email.data, password = form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('auth.login'))
+    title = 'New Account'
+  return render_template('auth/register.html', registration_form = form)
