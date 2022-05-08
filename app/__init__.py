a4f5ef8e3fe9f3559c92dcd8a70ccd1d3f1b2573
charles okunzo <okunzopeter01@gmail.com>
2022-05-08 11:58:36 +0300

    feature:addflask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+from sys import prefix
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -19,11 +25,15 @@ def create_app(config_name):
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
 
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
 
 
   # initialize extensions
   bootstrap.init_app(app)
   db.init_app(app)
+  login_manager.init_app(app)
 
 
   return app
