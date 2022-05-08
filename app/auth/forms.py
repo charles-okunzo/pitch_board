6ce8acd7df9619466f2f590ba21ad8b0f3ee3ba8
charles okunzo <okunzopeter01@gmail.com>
2022-05-08 18:27:56 +0300

    feature:add custom validation methods for email and username

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
+from ..models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,3 +10,13 @@ class RegistrationForm(FlaskForm):
   password = PasswordField('Password', validators=[DataRequired(), EqualTo('password_confirm', message='Passwords must match')])
   password_confirm = PasswordField('Confirm password', validators=[DataRequired()])
   submit = SubmitField('Sign Up')
+
+
+  def validate_username(self, data_field):
+    if User.query.filter_by(username = data_field.data):
+      raise AttributeError('An account with this email exists')
+
+
+  def validate_email(self, data_field):
+    if User.query.filter_by(email = data_field.data):
+      raise AttributeError('Username exists')
