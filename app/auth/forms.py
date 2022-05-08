6770866362defb2aa31c3064085f0e8966e18ae5
charles okunzo <okunzopeter01@gmail.com>
2022-05-08 20:43:22 +0300

    feature:login- add login class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 
@@ -14,9 +14,16 @@ class RegistrationForm(FlaskForm):
 
   def validate_username(self, data_field):
     if User.query.filter_by(username = data_field.data).first():
-      raise AttributeError('An account with this email exists')
+      raise ValidationError('An account with this email exists')
 
 
   def validate_email(self, data_field):
     if User.query.filter_by(email = data_field.data).first():
-      raise AttributeError('Username exists')
+      raise ValidationError('Username exists')
+
+
+class LogIn(FlaskForm):
+  email = StringField('Enter your email address:', validators=[DataRequired(), Email()])
+  password = PasswordField('Your password:', validators=[DataRequired()])
+  remember = BooleanField('Remember me')
+  submit = SubmitField('Log In')
