5783dd43d3258c97e753085faad9538dceba41ee
charles okunzo <okunzopeter01@gmail.com>
2022-05-08 10:55:24 +0300

    feature:password hash- add password hash and test runs

--- app/models.py
+++ app/models.py
@@ -1,10 +1,25 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
   username = db.Column(db.String(255), nullable = False, unique = True)
+  pass_secure = db.Column(db.String(128), unique = True, nullable = False)
+
+
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+  @password.setter
+  def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+
+
+  def verify_password(self, password):
+    return check_password_hash(self.pass_secure, password)
 
 
   def __repr__(self):
--- manage.py
+++ manage.py
@@ -16,6 +16,12 @@ def make_shell_context():
 migrate = Migrate(app)
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def tests():
+  import unittest
+  tests = unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
   manager.run()
