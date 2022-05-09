8d426a7a3ba87adb68c535491e8a0317dd5f94e4
charles okunzo <okunzopeter01@gmail.com>
2022-05-09 17:15:33 +0300

    feature:mail --add configurations for flask mail

--- config.py
+++ config.py
@@ -5,10 +5,16 @@ load_dotenv()
 
 
 class Config():
-  SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URL')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
   SECRET_KEY=os.environ.get('SECRET_KEY')
   UPLOADED_PHOTOS_DEST='app/static/photos'
+
+  MAIL_SERVER='smtp.googlemail.com'
+  MAIL_PORT=587
+  MAIL_USE_TLS=True
+  MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
 
 
 class ProdConfig(Config):
