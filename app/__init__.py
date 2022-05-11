94c6f71fe17d7bb8b15f6bcce392767f66c2649a
charles okunzo <okunzopeter01@gmail.com>
2022-05-11 23:05:46 +0300

    deployment updates

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
+import sys
+import logging
 
 
 photos = UploadSet('photos', IMAGES)
@@ -22,6 +24,9 @@ mail = Mail()
 def create_app(config_name):
   
   app = Flask(__name__)
+
+  app.logger.addHandler(logging.StreamHandler(sys.stdout))
+  app.logger.setLevel(logging.ERROR)
 
   #configurations
   from config import config_options
