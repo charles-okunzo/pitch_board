4a532a67dfb684817def6a5b354996477f65df67
charles okunzo <okunzopeter01@gmail.com>
2022-05-11 13:02:12 +0300

    deployment updates

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ load_dotenv()
 
 
 class Config():
-  # SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:okunzo254@localhost/pitch_board'
-  SQLALCHEMY_TRACK_MODIFICATIONS=False
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+  # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:okunzo254@localhost/pitch_board'
+  SQLALCHEMY_TRACK_MODIFICATIONS=True
   SECRET_KEY=os.environ.get('SECRET_KEY')
   UPLOADED_PHOTOS_DEST='app/static/photos'
 
   MAIL_SERVER='smtp.googlemail.com'
-  MAIL_PORT=587
+  MAIL_PORT=465
   MAIL_USE_TLS=True
   MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
   MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
@@ -28,6 +28,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
   DEBUG = True
 
 
