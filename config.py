b22d30ad98aa3752d1966784e9c25fad461db134
charles okunzo <okunzopeter01@gmail.com>
2022-05-11 22:59:08 +0300

    deployment updates

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config():
   # SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-  # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:okunzo254@localhost/pitch_board'
+  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:okunzo254@localhost/pitch_board'
   SQLALCHEMY_TRACK_MODIFICATIONS=True
   SECRET_KEY=os.environ.get('SECRET_KEY')
   UPLOADED_PHOTOS_DEST='app/static/photos'
